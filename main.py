e94e0ee667a1638a93d206d5c873b276703cd559
zeefeerkee <finekii@vk.com>
2022-10-25 23:39:31 +0500

    v.0.0.1

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import winsound
 
-k = 1.5
+
+def DigitsFixed(num, digits):
+    return f"{num:.{digits}f}"
+
+
 def is_correct_date(x, i):
     if i == 0:
         return x >= 0 and x <= 2.45
     elif i == 1:
         return x >= 0
+
+
+winsound.Beep(2000,500)
+print('(\t)'*3, 'Газодинамические функции v.0.0.1', '(\t)'*3, '\n', '*'*80)
 while True:
-    i = int(input('M -- 1, lambda -- 0 '))
+    i = int(input('M -- 1, λ -- 0 '))
     if i == 0 or i == 1:
         while True:
-            k = float(input('k = '))
+            k = float(input('\nk = '))
             if k > 1:
                 break
         while True:
@@ -21,7 +30,7 @@ while True:
                     l = float(input('\nM = '))
                     if is_correct_date(l,i) == True:
                         f = pow((k+1)*(l**2)/(2+(l**2)*(k-1)), 1/2)
-                        print('\nМах был преобразован в лямбду l(', l,',', k, ') = ', f)
+                        print('\nМах был преобразован в λ(', l,',', k, ') = ', f)
                         break
                 break
             else:
@@ -33,7 +42,11 @@ while True:
                 break
         break
     print('\nincorrect\n')
-print('jopa')
+tl = 1.0-pow(f,2)*(k-1.0)/(k+1.0)
+pl = pow(1-pow(f, 2)*(k-1)/(k+1), k/(k-1))
+el = pow(1-(k-1)/(k+1)*pow(f, 2), 1/(k-1))
+ql = f*pow((k+1)/2, 1/(k-1))*pow(1-pow(f, 2)*(k-1)/(k+1), 1/(k-1))
+print('\nτ(λ) = ', DigitsFixed(tl, 6), 'π(λ) = ', DigitsFixed(pl, 6), 'ε(λ) = ', DigitsFixed(el, 6), 'q(λ) = ', DigitsFixed(ql, 6))
 
 
 
